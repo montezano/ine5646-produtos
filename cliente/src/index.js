2a0fb202af04623d7db612d53f9f095b605bde9c
Pablo Rinco Montezano <pablomontezano@gmail.com>
2018-05-15 14:59:03 -0300

    modificado theme para dark

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componentes/App';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const M = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
     <App/>
   </MuiThemeProvider>
 )
